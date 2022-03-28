50e79f1b16a4a24a60b62243a22216c1b375bd0c
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-27 23:05:20 -0300

    add like and  isnull

---
+++ Codigos_SQL/isnull.sql
@@ -0,0 +1,6 @@
+use sakila;
+
+select * from address;
+
+select * from address 
+where address2 is null;
---
+++ Codigos_SQL/like.sql
@@ -0,0 +1,15 @@
+use sakila;
+
+select * from actor;
+
+select * from actor where first_name like 'A%';
+
+select * from actor where first_name like 'AL%';
+
+select * from actor where first_name like 'ALB%';
+
+select * from actor where first_name like 'alb%';
+
+select * from actor where first_name like 'alb%';
+
+select * from actor where first_name like '%o';
