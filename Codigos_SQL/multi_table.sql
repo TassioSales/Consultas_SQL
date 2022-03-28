c47d0dd8d5a5ab08ee8b0067dd092d34236d6d4c
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-28 16:00:05 -0300

    Create multi_table.sql

---
+++ Codigos_SQL/multi_table.sql
@@ -0,0 +1,18 @@
+use sakila;
+
+select * from customer 
+join payment 
+on customer.customer_id = payment.payment_id;
+
+select 
+	cus.customer_id, 
+    cus.first_name,
+    cus.last_name,
+    adr.address,
+    pay.rental_id,
+    pay.amount
+from customer cus
+join payment pay
+	on cus.customer_id = pay.payment_id
+join address adr 
+	on  cus.customer_id = adr.address_id;
